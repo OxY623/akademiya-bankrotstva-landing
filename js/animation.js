a09f0b07062c48aaf229b622a0ef27cc38a197fa
OxY623 <fuzzydoorproduction0@gmail.com>
2026-01-07 00:15:10 +0300

    feat: added animation hero

---
+++ js/animation.js
@@ -0,0 +1,35 @@
+const INCREASE_NUMBER_ANIMATION_SPEED = 40;
+let animation = false;
+
+const element = document.querySelector("#");
+
+function increaseNumberAnimationStep(i, element, endNumber) {
+  if (i <= endNumber) {
+    if (i === 4920) {
+      element.textContent = i + 80 + "+";
+    } else {
+      element.textContent = i;
+    }
+    i += 123;
+    setTimeout(function () {
+      increaseNumberAnimationStep(i, element, endNumber);
+    }, INCREASE_NUMBER_ANIMATION_SPEED);
+  }
+}
+
+function initIncreaseNumberAnimation(element) {
+  increaseNumberAnimationStep(0, element, 5000);
+}
+
+function updateScroll() {
+  if (!element) return;
+  const countElementPosition = element.offsetTop;
+  const windowBottomPosition = window.scrollY + window.innerHeight;
+
+  if (windowBottomPosition >= countElementPosition && !animation) {
+    initIncreaseNumberAnimation(element);
+    animation = true;
+  }
+}
+
+window.addEventListener("scroll", updateScroll);
